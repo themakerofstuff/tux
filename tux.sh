1ea04447fd3bf45c11a5e1195a2e2d2223456f12
Alman Khan <ak47.almank@gmail.com>
2024-08-23 12:42:00 +0100

    base

--- tux.sh
+++ tux.sh
@@ -149,7 +149,7 @@ tux_install() {
                 tux_error "Failed to run post-install for package ${pkg}"
                 tux_error "Log can be found at:"
                 echo $LOG_FILE
-                rm -rf $BUILD_DIR $DST
+                rm -rf $BUILD_DIR $DST $INDEX_DIR
                 exit 1
             fi
         fi
@@ -158,6 +158,7 @@ tux_install() {
 	    unset -f buildpkg
 	    unset -f installpkg
 	    unset -f postinstpkg
+        unset -f copypkgfiles
         tux_success "Successfully installed ${pkg}"
     done
 }
@@ -321,91 +322,102 @@ tux_bootstrap() {
     fi
     tux_info "Creating full root filesystem..."
     chroot $ROOT /usr/bin/env -i HOME=/root TERM="$TERM" PS1="(chroot)\u:\w$ " PATH=/usr/bin:/usr/sbin /bin/bash --login -e << "EOT"
-    mkdir -pv /{boot,home,mnt,opt,srv}
-    mkdir -pv /etc/{opt,sysconfig}
-    mkdir -pv /lib/firmware
-    mkdir -pv /media/{floppy,cdrom}
-    mkdir -pv /usr/{,local/}{include,src}
-    mkdir -pv /usr/lib/locale
-    mkdir -pv /usr/local/{bin,lib,sbin}
-    mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
-    mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
-    mkdir -pv /usr/{,local/}share/man/man{1..8}
-    mkdir -pv /var/{cache,local,log,mail,opt,spool}
-    mkdir -pv /var/lib/{color,misc,locate}
+mkdir -pv /{boot,home,mnt,opt,srv}
+mkdir -pv /etc/{opt,sysconfig}
+mkdir -pv /lib/firmware
+mkdir -pv /media/{floppy,cdrom}
+mkdir -pv /usr/{,local/}{include,src}
+mkdir -pv /usr/lib/locale
+mkdir -pv /usr/local/{bin,lib,sbin}
+mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -pv /usr/{,local/}share/man/man{1..8}
+mkdir -pv /var/{cache,local,log,mail,opt,spool}
+mkdir -pv /var/lib/{color,misc,locate}
 
-    [ -d "/var/run" ] || ln -sfv /run /var/run
-    [ -d "/var/lock" ] || ln -sfv /run/lock /var/lock
+[ -d "/var/run" ] || ln -sfv /run /var/run
+[ -d "/var/lock" ] || ln -sfv /run/lock /var/lock
 
-    install -dv -m 0750 /root
-    install -dv -m 1777 /tmp /var/tmp
-    [ -f "/etc/mtab" ] || ln -sv /proc/self/mounts /etc/mtab
-    cat > /etc/hosts << EOF
-    127.0.0.1  localhost $(hostname)
-    ::1        localhost
-    EOF
-    cat > /etc/passwd << "EOF"
-    root:x:0:0:root:/root:/bin/bash
-    bin:x:1:1:bin:/dev/null:/usr/bin/false
-    daemon:x:6:6:Daemon User:/dev/null:/usr/bin/false
-    messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/usr/bin/false
-    systemd-journal-gateway:x:73:73:systemd Journal Gateway:/:/usr/bin/false
-    systemd-journal-remote:x:74:74:systemd Journal Remote:/:/usr/bin/false
-    systemd-journal-upload:x:75:75:systemd Journal Upload:/:/usr/bin/false
-    systemd-network:x:76:76:systemd Network Management:/:/usr/bin/false
-    systemd-resolve:x:77:77:systemd Resolver:/:/usr/bin/false
-    systemd-timesync:x:78:78:systemd Time Synchronization:/:/usr/bin/false
-    systemd-coredump:x:79:79:systemd Core Dumper:/:/usr/bin/false
-    uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/usr/bin/false
-    systemd-oom:x:81:81:systemd Out Of Memory Daemon:/:/usr/bin/false
-    nobody:x:65534:65534:Unprivileged User:/dev/null:/usr/bin/false
-    EOF
-    cat > /etc/group << "EOF"
-    root:x:0:
-    bin:x:1:daemon
-    sys:x:2:
-    kmem:x:3:
-    tape:x:4:
-    tty:x:5:
-    daemon:x:6:
-    floppy:x:7:
-    disk:x:8:
-    lp:x:9:
-    dialout:x:10:
-    audio:x:11:
-    video:x:12:
-    utmp:x:13:
-    cdrom:x:15:
-    adm:x:16:
-    messagebus:x:18:
-    systemd-journal:x:23:
-    input:x:24:
-    mail:x:34:
-    kvm:x:61:
-    systemd-journal-gateway:x:73:
-    systemd-journal-remote:x:74:
-    systemd-journal-upload:x:75:
-    systemd-network:x:76:
-    systemd-resolve:x:77:
-    systemd-timesync:x:78:
-    systemd-coredump:x:79:
-    uuidd:x:80:
-    systemd-oom:x:81:
-    wheel:x:97:
-    users:x:999:
-    nogroup:x:65534:
-    EOF
-    localedef -i C -f UTF-8 C.UTF-8
-    EOT
+install -dv -m 0750 /root
+install -dv -m 1777 /tmp /var/tmp
+[ -f "/etc/mtab" ] || ln -sv /proc/self/mounts /etc/mtab
+cat > /etc/hosts << EOF
+127.0.0.1  localhost $(hostname)
+::1        localhost
+EOF
+cat > /etc/passwd << "EOF"
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/dev/null:/usr/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/usr/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/usr/bin/false
+systemd-journal-gateway:x:73:73:systemd Journal Gateway:/:/usr/bin/false
+systemd-journal-remote:x:74:74:systemd Journal Remote:/:/usr/bin/false
+systemd-journal-upload:x:75:75:systemd Journal Upload:/:/usr/bin/false
+systemd-network:x:76:76:systemd Network Management:/:/usr/bin/false
+systemd-resolve:x:77:77:systemd Resolver:/:/usr/bin/false
+systemd-timesync:x:78:78:systemd Time Synchronization:/:/usr/bin/false
+systemd-coredump:x:79:79:systemd Core Dumper:/:/usr/bin/false
+uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/usr/bin/false
+systemd-oom:x:81:81:systemd Out Of Memory Daemon:/:/usr/bin/false
+nobody:x:65534:65534:Unprivileged User:/dev/null:/usr/bin/false
+EOF
+cat > /etc/group << "EOF"
+root:x:0:
+bin:x:1:daemon
+sys:x:2:
+kmem:x:3:
+tape:x:4:
+tty:x:5:
+daemon:x:6:
+floppy:x:7:
+disk:x:8:
+lp:x:9:
+dialout:x:10:
+audio:x:11:
+video:x:12:
+utmp:x:13:
+cdrom:x:15:
+adm:x:16:
+messagebus:x:18:
+systemd-journal:x:23:
+input:x:24:
+mail:x:34:
+kvm:x:61:
+systemd-journal-gateway:x:73:
+systemd-journal-remote:x:74:
+systemd-journal-upload:x:75:
+systemd-network:x:76:
+systemd-resolve:x:77:
+systemd-timesync:x:78:
+systemd-coredump:x:79:
+uuidd:x:80:
+systemd-oom:x:81:
+wheel:x:97:
+users:x:999:
+nogroup:x:65534:
+EOF
+localedef -i C -f UTF-8 C.UTF-8
+EOT
     chroot $ROOT /usr/bin/env -i HOME=/root TERM="$TERM" PS1="(chroot)\u:\w$ " PATH=/usr/bin:/usr/sbin /bin/bash --login -e << "EOT"
-    touch /var/log/{btmp,lastlog,faillog,wtmp}
-    chgrp -v utmp /var/log/lastlog
-    chmod -v 664  /var/log/lastlog
-    chmod -v 600  /var/log/btmp
-    EOT
+touch /var/log/{btmp,lastlog,faillog,wtmp}
+chgrp -v utmp /var/log/lastlog
+chmod -v 664  /var/log/lastlog
+chmod -v 600  /var/log/btmp
+EOT
     tux_info "Building base-temptools..."
     chroot $ROOT /usr/bin/env -i HOME=/root TERM="$TERM" PS1="(chroot)\u:\w$ " PATH=/usr/bin:/usr/sbin /bin/bash --login -e << "EOT"
-    tux install base-temptools
+tux install base-temptools
+EOT
+    tux_info "Cleaning up temporary tools..."
+    chroot $ROOT /usr/bin/env -i HOME=/root TERM="$TERM" PS1="(chroot)\u:\w$ " PATH=/usr/bin:/usr/sbin /bin/bash --login -e << "EOT"
+rm -rf /usr/share/{info,man,doc}/*
+find /usr/{lib,libexec} -name \*.la -delete
+rm -rf /tools
+EOT
+    tux_info "Building base..."
+    chroot $ROOT /usr/bin/env -i HOME=/root TERM="$TERM" PS1="(chroot)\u:\w$ " PATH=/usr/bin:/usr/sbin /bin/bash --login -e << "EOT"
+tux install base
+EOT
 }
 
 if [ "$EUID" != "0" ]; then tux_error "This must be run as root"; exit 1; fi
